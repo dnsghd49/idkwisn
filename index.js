c0bc06dc9a982882112fca2bb1c5fb1ce91123b1
Aaron Lu <aaronnoh0409@gmail.com>
2021-01-16 00:40:20 -0500

    user question

--- index.js
+++ index.js
@@ -6,13 +6,44 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
     {
-        type: "",
-        name: "",
-        message: ""
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
     },{
-        type: "",
-        name: "",
-        message: ""
+        type: "input",
+        name: "email",
+        message: "What is your email address?"
+    },{
+        type: "input",
+        name: "title",
+        message: "What is your project's name?"
+    },{
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project"
+    },{
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+    },{
+        type: "input",
+        name: "installation",
+        message: "What command should be run to install dependencies?",
+        default: "npm i"
+    },{
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: "npm test"
+    },{
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?",
+    },{
+        type: "input",
+        name: "contributing",
+        message: "What does the user need to know about contributing to the repo?",
     }
 
 ];

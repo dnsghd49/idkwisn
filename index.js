41ca5b532e2be3dd046c580446ebf3a23be2a63e
Aaron Lu <aaronnoh0409@gmail.com>
2021-01-16 00:11:42 -0500

    basic functions and question setups

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "",
+        name: "",
+        message: ""
+    },{
+        type: "",
+        name: "",
+        message: ""
+    }
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+      
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions)
+    .then((inquirerResponses) => {
+      console.log("Generating README...");
+      writeToFile("README.md", generateMarkdown({...inquirerResponses}));
+    })
 }
 
 // function call to initialize program
